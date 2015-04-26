b271efb8ea56b9b8b53ca7b27acdf46d6ecf3c8a
darkson95 <sezer@karaoglanoglu.net>
2015-04-27 00:36:08 +0200

    Base

---
+++ worktime.js
@@ -0,0 +1,58 @@
+Router.route('/', function () { this.render('Home'); });
+Router.route('/about');
+Router.route('/admin');
+
+Times = new Mongo.Collection("times");
+var notCame;
+
+if (Meteor.isClient) {
+
+
+	Template.home.helpers({
+		times: function () {
+			return Times.find({worker: Meteor.userId()}, {sort: {createdAt: -1}});
+		}
+	});
+
+	alert(Times.find({worker: Meteor.userId()}, {sort: {createdAt: -1}, fields: {sort: 1}, limit: 1}));
+
+	Template.body.events({
+		"click #arrive": function (event) {
+			Meteor.call("arrive");
+		},
+		"click #leave": function (event) {
+			Meteor.call("leave");
+		}
+	});
+	
+	Accounts.ui.config({
+		passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+	});
+}
+
+Meteor.methods({
+	arrive: function () {
+		// Make sure the user is logged in before inserting a task
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+
+		Times.insert({
+			sort: "arriving",
+			createdAt: new Date(),
+			worker: Meteor.userId()
+		});
+	},
+	leave: function () {
+		// Make sure the user is logged in before inserting a task
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+
+		Times.insert({
+			sort: "leaving",
+			createdAt: new Date(),
+			worker: Meteor.userId()
+		});
+	}
+});
